0fa4116285b1b19f18a109138f12e974f0983911
sherlyjenifer <148056543+sherlyjenifer@users.noreply.github.com>
2023-10-16 15:37:20 +0530

    Add files via upload

---
+++ challenge1.2.py.py
@@ -0,0 +1,10 @@
+print(# Python prograck 
+year = 2000 
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".form y
+  
